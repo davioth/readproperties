e515d8c885bde9525b2338e12f4526d87c114748
Ubay Díaz Machín <davioth@gmail.com>
2018-01-20 13:42:06 +0000

    Inicializando el repositorio

---
+++ src/main/java/com/davioth/readproperties/ReadpropertiesApplication.java
@@ -0,0 +1,12 @@
+package com.davioth.readproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadpropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadpropertiesApplication.class, args);
+	}
+}
